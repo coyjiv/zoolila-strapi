5aa633bf8e1b1507c6bda4700c956ddc45039310
Daniil Shcherbakov <dshcherbakov@seamgroup.com>
2022-08-29 15:33:12 +0100

    initial commit

---
+++ src/api/home-dogs-slide/controllers/home-dogs-slide.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  home-dogs-slide controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::home-dogs-slide.home-dogs-slide');
