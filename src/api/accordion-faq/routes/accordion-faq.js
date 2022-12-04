a25a54f57c88936382efb633c672a568b887df48
Daniil Shcherbakov <dshcherbakov@seamgroup.com>
2022-12-04 14:48:40 +0000

    small fix

--- src/api/accordion-faq/routes/accordion-faq.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * accordion-faq router
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::accordion-faq.accordion-faq');
--- src/api/accordion-faq/services/accordion-faq.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * accordion-faq service
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::accordion-faq.accordion-faq');
