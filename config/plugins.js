e9aadfbae8949f76224b0b262e2ac808c3429ec7
Daniil Shcherbakov <dshcherbakov@seamgroup.com>
2022-12-09 13:43:25 +0000

    deepl removed due to an error

--- config/plugins.js
+++ config/plugins.js
@@ -1,25 +1,4 @@
 module.exports = ({ env }) => ({
-  deepl: {
-    enabled: true,
-    config: {
-      // your DeepL API key
-      apiKey: env('DEEPL_API_KEY'),
-      // whether to use the free or paid api, default true
-      freeApi: true,
-      // Which field types are translated (default string, text, richtext, components and dynamiczones)
-      translatedFieldTypes: [
-        'string',
-        'text',
-        'richtext',
-        'component',
-        'dynamiczone',
-      ],
-      // If relations should be translated (default true)
-      translateRelations: true,
-      // You can define a custom glossary to be used here (see https://www.deepl.com/docs-api/managing-glossaries/)
-      // glossaryId: 'customGlossary',
-    },
-  },
     upload: {
       config: {
         provider: 'cloudinary',
